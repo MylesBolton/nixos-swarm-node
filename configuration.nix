562bddb5af41ec49343ccbd2781ea3b5fc8d8ec2
Myles Bolton <github@mylesbolton.com>
2024-05-24 10:27:48 +0100

    added rpi4 config

--- configuration.nix
+++ configuration.nix
@@ -17,9 +17,22 @@
     ./users.nix
   ];
 
-  boot.loader = {
-    efi.canTouchEfiVariables = true;
-    systemd-boot.enable = true;
+
+  boot = {
+    kernelPackages = pkgs.linuxKernel.packages.linux_rpi4;
+    initrd.availableKernelModules = [ "xhci_pci" "usbhid" "usb_storage" ];
+    loader = {
+      grub.enable = false;
+      generic-extlinux-compatible.enable = true;
+    };
+  };
+
+  fileSystems = {
+    "/" = {
+      device = "/dev/disk/by-label/NIXOS_SD";
+      fsType = "ext4";
+      options = [ "noatime" ];
+    };
   };
 
   nixpkgs.config = {
